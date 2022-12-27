3d275de24752637d68c97df6dd63c1297132ca17
rhitambanerjee <rb2403@it.jgec.ac.in>
2022-12-27 18:14:21 +0530

    Initial Commit

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,31 @@
+
+import { styled, Box, Typography } from '@mui/material';
+
+const Image = styled(Box)`
+    width: 100%;
+    background: url(https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg) center/55% repeat-x #000;
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Heading = styled(Typography)`
+    font-size: 70px;
+    color: #FFFFFF;
+    line-height: 1
+`;
+
+
+
+const Banner = () => {
+    
+    return (
+        <Image>
+            <Heading>BLOG</Heading>
+        </Image>
+    )
+}
+
+export default Banner;
